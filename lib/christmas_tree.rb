30f5e468d6a955940b162737d16ff3ec6bf4335a
Vinicíus Leonárdo Navarra <52398261+ViniciusLNavarra@users.noreply.github.com>
2021-08-28 00:20:44 +0200

    Finished Code

--- lib/christmas_tree.rb
+++ lib/christmas_tree.rb
@@ -2,9 +2,20 @@
 
 class ChristmasTree
   TREE_CHARACTER = 'X'
-
-  def draw(height)
-    # TODO: implement logic
-    height
+  def draw(height, stand=false, star=false)
+    i = 1
+    treestr = ""
+    if (star)
+      treestr += ' ' * (height - 1) + '*' + ' ' * (height - 1) + "\n"
+    end
+    until i > height
+      treestr += ' ' * (height - i) + TREE_CHARACTER * (2 * i - 1) + ' ' * (height - i) + "\n"
+      i += 1
+    end
+    if (stand)
+      treestr += ' ' * (height - 1) + '|' + ' ' * (height - 1) + "\n"
+    end
+    puts treestr
+    return treestr
   end
 end
