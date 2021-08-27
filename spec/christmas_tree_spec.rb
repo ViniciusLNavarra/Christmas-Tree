85542e8f8bc5e8b9efc002a0b942fc396aae5de1
Vinicíus Leonárdo Navarra <52398261+ViniciusLNavarra@users.noreply.github.com>
2021-08-28 00:22:25 +0200

    Finished Tests

--- spec/christmas_tree_spec.rb
+++ spec/christmas_tree_spec.rb
@@ -3,17 +3,38 @@
 require 'christmas_tree'
 
 RSpec.describe ChristmasTree do
-  subject(:christmas_tree) { described_class.new.draw(height) }
-
-  let(:height) { 5 }
+  subject(:christmas_tree) { described_class.new.draw(height)}
+  
+  let(:height) {5}
   let(:tree_characters_count) { christmas_tree.count(described_class::TREE_CHARACTER) }
 
-  # TODO: find a way to improve this test. Currently it just counts the occurences of TREE_CHARACTER
   it 'draws the christmas tree correctly' do
-    expect(tree_characters_count).to eq(height ** 2)
+    i=1
+    for line in christmas_tree.each_line
+      expect(line.count('X')).to eq(i)
+      i+=2
+    end
   end
 
   context 'with star and tree stand' do
-    # TODO: write a test for the advanced task
+    subject(:christmas_tree) {described_class.new.draw(height, stand, star)}
+
+    let(:height) {10}
+    let(:star) {true}
+    let(:stand) {true}
+
+    it 'draws the christmas tree correctly' do
+      i=1
+      for line in christmas_tree.each_line
+        if (line.count('*')>=1)    
+          expect(christmas_tree.count('*')).to eq(1)
+        elsif (line.count('|')>=1)    
+          expect(christmas_tree.count('|')).to eq(1)
+        else
+          expect(line.count('X')).to eq(i)
+          i+=2
+        end
+      end
+    end
   end
 end
